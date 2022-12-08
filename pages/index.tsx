2e509ec56dce11dff8e969babe1a81f3946f15fd
zhangxueyu <1501109542@qq.com>
2022-12-08 18:09:01 +0800

    feat: profile page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import PersonProfile from '../compoents/PersonProfile'
-import { useEvmNativeBalance } from '@moralisweb3/next';
-
+import { useEvmNativeBalance } from '@moralisweb3/next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
-  const address = '0xc14B8187368738532c71318cD77e7e28Ed9d53d3';
-  const { data: nativeBalance } = useEvmNativeBalance({ address });
+  const address = '0xc14B8187368738532c71318cD77e7e28Ed9d53d3'
+  const { data: nativeBalance } = useEvmNativeBalance({ address })
   //connect wallet and live in data
 
   return (
@@ -17,11 +17,18 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-          <h3>Wallet: {address}</h3>
-          <h3>Native Balance: {nativeBalance?.balance.ether} ETH</h3>
+        <h3>Wallet: {address}</h3>
+        <h3>Native Balance: {nativeBalance?.balance.ether} ETH</h3>
       </div>
       <div>begin here</div>
-      <PersonProfile></PersonProfile>
+      <div className="flex gap-2">
+        <Link href="/UserProfile">
+          <div className="p-2 border rounded-lg">to user-profile</div>
+        </Link>
+        <Link href="/OrganizationProfile">
+          <div className="p-2 border rounded-lg">to organization-profile</div>
+        </Link>
+      </div>
     </div>
   )
 }

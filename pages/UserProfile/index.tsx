434da5cebefdd9594b6e78d285778e3141969d7f
zhangxueyu <1501109542@qq.com>
2022-12-10 12:39:54 +0800

    feat: avatar

--- pages/UserProfile/index.tsx
+++ pages/UserProfile/index.tsx
@@ -1,13 +1,33 @@
 import Link from 'next/link'
 import React from 'react'
 
+import { BiEdit } from 'react-icons/bi'
+
 const UserProfile = () => {
   return (
     <div className="flex justify-center">
       <div className="w-[1100px] flex justify-between pt-14">
         <div className="flex flex-col h-[668px] justify-between">
-          <div className="w-[350px] h-[250px] bg-[#C9CDD4] rounded-lg"></div>
-          <div className="w-[350px] h-[390px] bg-[#C9CDD4] rounded-lg"></div>
+          <div className="w-[350px] h-[250px] bg-[#F5F5F5] rounded-lg p-4">
+            <div className="w-full flex justify-between">
+              <div className="avatar">
+                <div className="w-[88px] h-[88px] rounded-[50%]">
+                  <img src="https://placeimg.com/192/192/people" />
+                </div>
+              </div>
+              <div className="w-[150px] my-auto">
+                <div className="font-bold">Richard</div>
+                <div className="text-[#333333] text-xs">0x7812345667</div>
+              </div>
+              <div className="flex items-center cursor-pointer rounded-full bg-[#C9CDD4] h-7 w-7 my-auto text-center">
+                <BiEdit color="white" size={20} className="m-auto" />
+              </div>
+            </div>
+            <div className="">follow</div>
+            <div className="">introduce</div>
+            <div className="">other link</div>
+          </div>
+          <div className="w-[350px] h-[390px] bg-[#F5F5F5] rounded-lg"></div>
         </div>
         <div className="w-[700px]">
           <div className="flex justify-evenly">

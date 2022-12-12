eb3d46db81800ae3c44b40a3c3df6dde31933c56
zhangxueyu <1501109542@qq.com>
2022-12-12 15:22:12 +0800

    fix

--- pages/UserProfile/Feed.tsx
+++ pages/UserProfile/Feed.tsx
@@ -4,96 +4,40 @@ import { UserLayout } from '../../components/layouts'
 const Feed: NextPageWithLayout = () => {
   return (
     <div className="flex flex-col">
-      <div className="w-[700px] h-[180px] mt-8 bg-[#F5F5F5] rounded-xl p-6">
-        <div className="flex justify-between">
-          <div>
-            PEZ got <span className="font-bold">CN MetAverse</span>
-          </div>
-          <div className="text-[#999]">May 12,2022</div>
-        </div>
-        <div className="flex justify-between mt-7">
-          <div className="w-[88px]">
-            <img
-              className="rounded-[50%] cursor-pointer"
-              src="https://placeimg.com/192/192/people"
-              alt=""
-            />
-          </div>
-          <div className="w-[550px]">
-            <div className="w-[80px] h-[30px] bg-[#C9CDD4] rounded-xl f-c-c cursor-pointer">
-              Skill
+      {[1, 2, 3].map(() => {
+        return (
+          <div className="w-[700px] h-[180px] mt-8 bg-[#F5F5F5] rounded-xl p-6">
+            <div className="flex justify-between">
+              <div>
+                PEZ got <span className="font-bold">CN MetAverse</span>
+              </div>
+              <div className="text-[#999]">May 12,2022</div>
             </div>
-            <div className="text-[12px] mt-3 line-clamp-2">
-              CN Memetaverse is the creative social group in China. It has a
-              large community for making and dicussing meme, jokes and fun
-              stories of Chinese pop clutureand blockchain industry. CN
-              Memetaverse is the creative social group in China. It has a large
-              community for making and dicussing meme, jokes and fun stories of
-              Chinese pop clutureand blockchain industry.
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="w-[700px] h-[180px] mt-8 bg-[#F5F5F5] rounded-xl p-6">
-        <div className="flex justify-between">
-          <div>
-            PEZ got <span className="font-bold">CN MetAverse</span>
-          </div>
-          <div className="text-[#999]">May 12,2022</div>
-        </div>
-        <div className="flex justify-between mt-7">
-          <div className="w-[88px]">
-            <img
-              className="rounded-[50%] cursor-pointer"
-              src="https://placeimg.com/192/192/people"
-              alt=""
-            />
-          </div>
-          <div className="w-[550px]">
-            <div className="w-[80px] h-[30px] bg-[#C9CDD4] rounded-xl f-c-c cursor-pointer">
-              Skill
-            </div>
-            <div className="text-[12px] mt-3 line-clamp-2">
-              CN Memetaverse is the creative social group in China. It has a
-              large community for making and dicussing meme, jokes and fun
-              stories of Chinese pop clutureand blockchain industry. CN
-              Memetaverse is the creative social group in China. It has a large
-              community for making and dicussing meme, jokes and fun stories of
-              Chinese pop clutureand blockchain industry.
+            <div className="flex justify-between mt-7">
+              <div className="w-[88px]">
+                <img
+                  className="rounded-[50%] cursor-pointer"
+                  src="https://placeimg.com/192/192/people"
+                  alt=""
+                />
+              </div>
+              <div className="w-[550px]">
+                <div className="w-[80px] h-[30px] bg-[#C9CDD4] rounded-xl f-c-c cursor-pointer">
+                  Skill
+                </div>
+                <div className="text-[12px] mt-3 line-clamp-2">
+                  CN Memetaverse is the creative social group in China. It has a
+                  large community for making and dicussing meme, jokes and fun
+                  stories of Chinese pop clutureand blockchain industry. CN
+                  Memetaverse is the creative social group in China. It has a
+                  large community for making and dicussing meme, jokes and fun
+                  stories of Chinese pop clutureand blockchain industry.
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="w-[700px] h-[180px] mt-8 bg-[#F5F5F5] rounded-xl p-6">
-        <div className="flex justify-between">
-          <div>
-            PEZ got <span className="font-bold">CN MetAverse</span>
-          </div>
-          <div className="text-[#999]">May 12,2022</div>
-        </div>
-        <div className="flex justify-between mt-7">
-          <div className="w-[88px]">
-            <img
-              className="rounded-[50%] cursor-pointer"
-              src="https://placeimg.com/192/192/people"
-              alt=""
-            />
-          </div>
-          <div className="w-[550px]">
-            <div className="w-[80px] h-[30px] bg-[#C9CDD4] rounded-xl f-c-c cursor-pointer">
-              Skill
-            </div>
-            <div className="text-[12px] mt-3 line-clamp-2">
-              CN Memetaverse is the creative social group in China. It has a
-              large community for making and dicussing meme, jokes and fun
-              stories of Chinese pop clutureand blockchain industry. CN
-              Memetaverse is the creative social group in China. It has a large
-              community for making and dicussing meme, jokes and fun stories of
-              Chinese pop clutureand blockchain industry.
-            </div>
-          </div>
-        </div>
-      </div>
+        )
+      })}
     </div>
   )
 }
--- pages/UserProfile/Projects.tsx
+++ pages/UserProfile/Projects.tsx
@@ -5,116 +5,20 @@ import { AiOutlineEyeInvisible } from 'react-icons/ai'
 const Projects: NextPageWithLayout = () => {
   return (
     <div className="flex flex-wrap gap-12 mt-8">
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
+        return (
+          <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
+            <div className="w-full h-[160px] bg-[#C9CDD4] relative">
+              <div className="absolute top-1 right-2 hidden group-hover:block">
+                <AiOutlineEyeInvisible />
+              </div>
+            </div>
+            <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
+              CN Memetaverse Project Certificate
+            </div>
           </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
-      <div className="w-[200px] h-[240px] group rounded-xl overflow-clip cursor-pointer hover:shadow-2xl transition duration-300">
-        <div className="w-full h-[160px] bg-[#C9CDD4] relative">
-          <div className="absolute top-1 right-2 hidden group-hover:block">
-            <AiOutlineEyeInvisible />
-          </div>
-        </div>
-        <div className="w-full h-[80px] bg-[#F5F5F5] p-3 text-[14px]">
-          CN Memetaverse Project Certificate
-        </div>
-      </div>
+        )
+      })}
     </div>
   )
 }

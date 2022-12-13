104f164e260b9a8a76e47835edbff4e3992b7fe3
zhangxueyu <1501109542@qq.com>
2022-12-13 17:09:29 +0800

    feat

--- contexts/Collection.tsx
+++ contexts/Collection.tsx
@@ -3,6 +3,10 @@ import { createContext } from 'react'
 interface CollectionProps {
   collectionName: string
   setCollectionName: (name: string) => void
+  type: string
+  setType: (type: string) => void
+  description: string
+  setDescription: (description: string) => void
 }
 
 export default createContext({} as Partial<CollectionProps>)

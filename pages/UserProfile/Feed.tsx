12e42a288fae803b620ef84598f81633491e3fa6
wang-xin-yuan-001 <71478598+wang-xin-yuan-001@users.noreply.github.com>
2022-12-19 15:11:39 +0800

    feat: get all nft

--- pages/UserProfile/Feed.tsx
+++ pages/UserProfile/Feed.tsx
@@ -1,10 +1,32 @@
 import type { NextPageWithLayout } from "../_app";
 import { UserLayout } from "../../components/layouts";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import axios from "axios";
 
 const Feed: NextPageWithLayout = () => {
+  const options = {
+    method: "GET",
+    url: "https://deep-index.moralis.io/api/v2/0xc14B8187368738532c71318cD77e7e28Ed9d53d3/nft",
+    params: { chain: "polygon", format: "decimal", normalizeMetadata: "false" },
+    headers: {
+      accept: "application/json",
+      "X-API-Key":
+        "EdmOJ0JOgp5FF1VXNdxHC5wtzovAB9zIemMRZJ7VsM4Xo5WnDdZ16ZU6Iy0a8kry",
+    },
+  };
+
+  //get all nft
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
   return (
     <div className="flex flex-col">
-      {[1, 2, 3].map(item => {
+      {[1, 2, 3].map((item) => {
         return (
           <div
             key={item}

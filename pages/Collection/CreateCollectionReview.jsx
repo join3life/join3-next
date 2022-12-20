cc43b2fd854e2c279ed4326dfaa900debe3dd7a2
Demian <sodaoo@qq.com>
2022-12-20 19:31:03 +0800

    demian upload

--- pages/Collection/CreateCollectionReview.jsx
+++ pages/Collection/CreateCollectionReview.jsx
@@ -61,20 +61,57 @@ const CreateCollection = () => {
     console.log(`selected ${value}`);
   };
 
-  const successful = async () => {
+
+  const getaddress = async () => {
     try {
       const { ethereum } = window;
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const contract = new ethers.Contract(
-          contractAddress,
-          contractABI,
+          contractAddress, contractABI,
           signer
         );
-        const res = await contract.initCollection("tst", "test");
-        console.log(res);
-        const address = await contract.getColAddressByName("tst");
+        console.log('contractAddress', contractAddress, contractABI)
+        const addr = await contract.getColAddressByName("JOidkkdn3skd");
+        console.log('addr', addr)
+      }
+    } catch (err) {
+      console.log("error: ", err);
+    }
+
+  }
+  const successful = async () => {
+    // try {
+    //   const { ethereum } = window;
+    //   if (ethereum) {
+    //     const provider = new ethers.providers.Web3Provider(ethereum);
+    //     const signer = provider.getSigner();
+    //     const contract = new ethers.Contract(
+    //       contractAddress,
+    //       contractABI,
+    //       signer
+    //     );
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          contractAddress, contractABI,
+          signer
+        );
+        console.log('contractAddress', contractAddress, contractABI)
+        const res = await contract.initCollection(
+          "JOidkkdn3skd",  // Collection name
+          "symbol"  // ERC721 的 symbol，用户可以不填这个，没啥用
+        )
+        console.log('res', res)
+
+        // const address = await contract.getColAddressByName("Join2232212");
+        // console.log('address  ', address)
+        return;
+
 
         //todo 获取org的名字，获取type类型
         const orgId = info._id;
@@ -129,6 +166,12 @@ const CreateCollection = () => {
           Create
         </div>
       </div>
+      <div className="f-c-c mt-4">
+        <div className="btn" onClick={() => getaddress()}>
+          getAddress
+        </div>
+      </div>
+
     </div>
   );
 };
--- utils/constants.js
+++ utils/constants.js
@@ -9,7 +9,7 @@ export const contractAddress = "0xba8a4e4152902988170514b4f4eb125a4af8aad3" // �
 
 /* 测试 Factory 合约 */
 import abi from "./Factory.json";
-export const contractAddress = "0x6eb03f36d7759debafb3ad08ea637a47af2627c8"; // 测试合约的构造函数 */
+export const contractAddress = "0x634a6446100b2bf57c184d269604f6bc554a20f3"; // 测试合约的构造函数 */
 export const contractABI = abi.abi;
 export const NFT_STORAGE_API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDEwMDc1ZDQ2MDUzQTE2QmIxYjJhRDAxZDRGMTBFY2Y1N2ExRjlmNzUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2ODEzNTIwNzU4NywibmFtZSI6IkFwdG9zTkZUTWFya2V0In0.Ne0SSYDqLSotTnmyWUz2Vh6P4gMKj4K7VmtM9_Ml-ho";

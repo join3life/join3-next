b4fb893defebb3892fda3be8c62617365c1b4f3e
wang-xin-yuan-001 <71478598+wang-xin-yuan-001@users.noreply.github.com>
2022-12-21 01:27:15 +0800

    fix:build

--- contexts/Organization.ts
+++ contexts/Organization.ts
@@ -3,12 +3,6 @@ import { createContext } from "react";
 interface OrganizationProps {
   info: any;
   setInfo: (info: any) => void;
-  skills: any;
-  setSkills: (skills: any) => void;
-  events: any;
-  setEvents: (events: any) => void;
-  projects: any;
-  setProjects: (peojects: any) => void;
 }
 
 export default createContext({} as OrganizationProps);

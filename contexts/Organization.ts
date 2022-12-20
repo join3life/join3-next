c1e0ed6cc7716c5718f2d97663629b3d1a783c0d
wang-xin-yuan-001 <71478598+wang-xin-yuan-001@users.noreply.github.com>
2022-12-21 01:24:34 +0800

    fix: build

--- contexts/Organization.ts
+++ contexts/Organization.ts
@@ -4,8 +4,11 @@ interface OrganizationProps {
   info: any;
   setInfo: (info: any) => void;
   skills: any;
+  setSkills: (skills: any) => void;
   events: any;
+  setEvents: (events: any) => void;
   projects: any;
+  setProjects: (peojects: any) => void;
 }
 
 export default createContext({} as OrganizationProps);
